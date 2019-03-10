56d22f8c5630b9e5387b79d00f3e211b54ce28f5
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-03-10 07:37:00 -0700

    Update ThemePreview.plugin.js

--- LazyPlugins/ThemePreview.plugin.js
+++ LazyPlugins/ThemePreview.plugin.js
@@ -11,7 +11,7 @@ class ThemePreview {
         return 'Preview themes posted in #Theme-repo, and direct links that ends with CSS including directly uploaded files or [https://betterdiscord.net/ghdl?id=] link using context menu.';
     }
     getSettingsPanel() {
-        const panel = $('<form>').addClass('form').css('width', '100%');
+		const panel = $('<form>').addClass('form').css('width', '100%');
         if (this.initialized) this.generatePanel(panel);
         return panel[0];
     }
@@ -25,26 +25,26 @@ class ThemePreview {
         return `https://raw.githubusercontent.com/Modder4869/LazyStuff/master/LazyPlugins/${this.getName()}.plugin.js`;
     }
     constructor() {
-        this.request = require('request');
-		this.initialized = false;
-		this.minimumMilliseconds = 1000; //For easy modifiction for the settings panel.
-		this.maximumMilliseconds = 10000; //For easy modifiction for the settings panel.
-		this.bodyShowLog = false; //Just console logs the contents of the themes, that are previewed. Good for looking at the contents of a theme without downloading it as a file.
-        this.default = {
-            delay: false,
-            ms: 3000
-        };
-        this.settings = {
-            delay: false,
-            ms: 3000
-        };
+        this.request=require('request');
+		this.initialized=false;
+		this.milliseconds={ //For easy modifiction for the settings panel.
+			min:1000,
+			max:10000
+		}
         this.previewSheet;
         this.themeCSS;
         this.themeUrl;
-    }
+	}
+	get default(){ //when regenerating settings, the settings panel likes to change the default settings for some reason. This is the fix.
+		return{
+            delay:false,
+			ms:3000,
+			showBodyLog:false
+		}
+	}
 	load() {
-		let libraryScript = document.getElementById('zeresLibraryScript'), self = this;
-		if (!libraryScript) {
+		let libraryScript=document.getElementById('zeresLibraryScript'),self=this;
+		if (!libraryScript){
 			libraryScript = document.createElement('script');
 			libraryScript.setAttribute('type', 'text/javascript');
 			/*In part borrowed from Zere, so it redirects the user to download the Lib if it does not load correctly and the user does not have it.*/
@@ -97,7 +97,7 @@ class ThemePreview {
             url: url
         }, (error, response, body) => {
             this.themeCSS = body.substring(body.indexOf("\n") + 1);
-            if(this.bodyShowLog === true){console.log(body);}
+            if(this.settings.showBodyLog === true){console.log(body);}
             ZLibrary.Toasts.show('loaded', {
                 type: "success"
             });
@@ -157,40 +157,40 @@ class ThemePreview {
 		ZLibrary.PluginUtilities.saveSettings(this.getName(), this.settings);
 	}
 	loadSettings() {
-		ZLibrary.PluginUtilities.loadSettings(this.getName(), this.settings);
+		this.settings=ZLibrary.PluginUtilities.loadSettings(this.getName(), this.default);
 	}
     generatePanel(panel) { //does not use the SettingGroup callback so it can check/limit inputs.
-        new ZLibrary.Settings.SettingGroup('Preview Settings', {collapsible: true, shown: true}).appendTo(panel).append(
-            new ZLibrary.Settings.Switch('Preview Reset', 'Automatically reset the Theme Preview after a delay.', this.settings.delay, (i) => {
+        new ZLibrary.Settings.SettingGroup('Preview Settings',{collapsible:true,shown:true}).appendTo(panel).append(
+            new ZLibrary.Settings.Switch('Preview Reset','Automatically reset the Theme Preview after a delay.',this.settings.delay,(i)=>{
 				this.settings.delay = i;
 				this.saveSettings();
                 this.removeListeners();
                 this.addListeners();
 			}),
-			new ZLibrary.Settings.Textbox('Preview Reset Delay', 'How long to wait before resetting the Theme Preview. 1000ms = 1 second, for a minimum of 1 second and a maximum of 10 seconds.', this.settings.ms, (i) => {
+			new ZLibrary.Settings.Textbox('Preview Reset Delay','How long to wait before resetting the Theme Preview. 1000ms = 1 second, for a minimum of 1 second and a maximum of 10 seconds.',this.settings.ms,(i)=>{
 				let x = parseInt(i, 10);
-				this.removeListeners();
-				this.addListeners();
-				if (x !== NaN && this.minimumMilliseconds <= x && x <= this.maximumMilliseconds) { //Restricts inputs to numbers and limits (min/max) the seconds the user can input.
+				if (x!==NaN&&this.milliseconds.min<=x&&x<=this.milliseconds.max) { //Restricts inputs to numbers and limits (min/max) the seconds the user can input.
 					this.settings.ms = i;
 					this.saveSettings();
-				}else if (i === '' || x < this.minimumMilliseconds) {//Allows the textbox to be empty and below the minimum amount without regenerating the panel, removing a bit of irritation.
+					this.removeListeners();
+					this.addListeners();
+				}else if(i===''||x<this.milliseconds.min) {//Allows the textbox to be empty and below the minimum amount without regenerating the panel, removing a bit of irritation.
 				}else{ //Regenerate the panel when on incorrect input, if you have got a better way go for it.
 					this.regeneratePanel(panel);
 				}
-			}));
+			}),
+			new ZLibrary.Settings.Switch('Log Theme File In Console','Logs the theme file that is previewed in the developer tools console.',this.settings.showBodyLog,(i)=>{
+				this.settings.showBodyLog = i;
+				this.saveSettings();
+			})
+		);
 
-        const resetButton = $('<button>', {
-            type: 'button',
-            text: 'Reset To Default',
-            style: 'float: right;'
-        }).on('click.reset', () => {
-            for (const key in this.default) {
-                this.settings[key] = this.default[key];
-            }
+		const resetButton=$('<button>',{type:'button',text:'Reset To Default',style:'float: right;'})
+		.click(function(){
+			this.settings=this.default;
+			this.saveSettings();
 			this.regeneratePanel(panel);
-        });
-
+		}.bind(this));
         panel.append(resetButton);
     }
     stop() {

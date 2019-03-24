492699a43dd6a4112801f7d3cb6b9d9887b71698
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-03-24 08:20:10 -0700

    Update DeveloperUtils.plugin.js

--- LazyPlugins/DeveloperUtils.plugin.js
+++ LazyPlugins/DeveloperUtils.plugin.js
@@ -52,8 +52,9 @@ class DeveloperUtils {
 		}
     }
     start() {
-        if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
-      this.initialize()
+		let libraryScript=document.getElementById('zeresLibraryScript');
+		if(typeof ZLibrary==="object")this.initialize();
+		else libraryScript.addEventListener('load',()=>this.initialize());
     }
 
     initialize() {

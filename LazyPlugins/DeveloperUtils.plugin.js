66bc06b9217c4ee7e3b426851ab4906dca24e3f9
CompletelyUnbelievable <39345185+CompletelyUnbelievable@users.noreply.github.com>
2019-03-24 08:01:30 -0700

    Update DeveloperUtils.plugin.js

--- LazyPlugins/DeveloperUtils.plugin.js
+++ LazyPlugins/DeveloperUtils.plugin.js
@@ -1,4 +1,4 @@
-//META{"name":"DeveloperUtils"}*//
+//META{"name":"DeveloperUtils","source":"https://github.com/Modder4869/LazyStuff/blob/master/LazyPlugins/DeveloperUtils.plugin.js","website":"https://www.github.com/Modder4869"}*//
 class DeveloperUtils {
     getName() {
         return 'DeveloperUtilities';
@@ -7,10 +7,10 @@ class DeveloperUtils {
         return 'devutils';
     }
     getDescription() {
-        return 'allows you to inspect elements with alt + rightclick , and adds shortcut in context menu';
+        return 'Allows you to inspect elements with alt + rightclick, and adds shortcut in context menu';
     }
     getVersion() {
-        return '0.1.2 ';
+        return '0.1.3';
     }
     getAuthor() {
         return 'Modder4869';
@@ -40,7 +40,16 @@ class DeveloperUtils {
         ZLibrary.PluginUtilities.saveSettings(this.getName(), this.settings);
     }
     load() {
-
+		let libraryScript=document.getElementById('zeresLibraryScript');
+		if(!libraryScript){
+			libraryScript=document.createElement('script');
+			libraryScript.setAttribute('type','text/javascript');
+			/*In part borrowed from Zere, so it redirects the user to download the Lib if it does not load correctly and the user does not have the plugin version of the lib.*/
+			libraryScript.addEventListener("error",function(){if(typeof ZLibrary==="undefined"){window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing and could not be loaded.<br /><br /><a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);}}.bind(this));
+			libraryScript.setAttribute('src','https://rauenzi.github.io/BDPluginLibrary/release/ZLibrary.js');
+			libraryScript.setAttribute('id','zeresLibraryScript');
+			document.head.appendChild(libraryScript);
+		}
     }
     start() {
         if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
@@ -82,10 +91,10 @@ class DeveloperUtils {
 
 
     addContextMenuItems(e) {
-        let CSSRules = getMatchedCSSRules(e.toElement);
-            let context = document.querySelector('.contextMenu-HLZMGh');
-        if (!CSSRules) return;
-        let CSSRule = CSSRules.item(CSSRules.length - 1);
+        let CSSRules = this.getMatchedCSSRules(e.toElement);
+        let context = document.querySelector('.contextMenu-HLZMGh');
+		if(!CSSRules.length)return;
+        let CSSRule = CSSRules[CSSRules.length-1];
         let currentWin = this.currentWindow;
         let subMenu = new ZLibrary.ContextMenu.SubMenuItem("DevUtils", new ZLibrary.ContextMenu.Menu(false).addItems(
 
@@ -147,7 +156,34 @@ class DeveloperUtils {
         }
         if (context.classList.contains("plugin-context-menu")) return;
         $(context).find('.itemGroup-1tL0uz').last().append(testGroup.element);
-    }
+	}
+
+	getMatchedCSSRules(element) {
+		let matching=[],sheets=document.styleSheets;
+		function loopRules(rules){
+			for(let rule of rules){
+				if(rule instanceof CSSMediaRule){
+					if(window.matchMedia(rule.conditionText).matches){
+						loopRules(rule.cssRules);
+					}
+				}else if(rule instanceof CSSStyleRule){
+					if(element.matches(rule.selectorText)){
+						matching.push(rule);
+					}
+				}
+			}
+		};
+		for(let sheet of sheets){
+			try{
+				loopRules(sheet.cssRules);
+			}catch(e){
+				//Easiest way to fix an issue of a stylesheet not having a cssRules property. Unless you want to check every property name it has with object keys.
+			}
+		}
+		//console.log(matching); //Debug.
+		return matching;
+	}//Modified, based on https://stackoverflow.com/questions/2952667/find-all-css-rules-that-apply-to-an-element
+
     getSettingsPanel() {
         var panel = $("<form>").addClass("form").css("width", "100%");
         if (this.initialized) this.generateSettings(panel);
